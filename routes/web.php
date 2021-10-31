29cfa3fed9011246167e0747e7a62e3bc7be1514
jing yuan chen <3a732031@gm.student.ncut.edu.tw>
2021-10-31 20:56:37 +0800

    新增Route且每個對應一個獨立的Controller中的index方法

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\NewsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
-
+});*/
+Route::get('/',[HomeController::class,'index'])->name("home.index");
+Route::get('about',[AboutController::class,'index'])->name("about.index");
+Route::get('news',[NewsController::class,'index'])->name("news.index");
